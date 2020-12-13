ae3db9b435eef8c3caf11376764568e60fb227ac
Татьяна Останина <ostanina-2005@mail.ru>
2020-12-13 16:17:22 +0300

    Создание презентации

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-import PyQt5
-import pptx
+from PyQt5 import QtWidgets
+import sys
+from pptx import Presentation
+from PyQt5 import uic
+
+
+class MyWidget(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Greeting window.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.pushButton.clicked.connect(self.create_presentation)
+
+    def create_presentation(self):
+        print('Hello')
+        self.presantation = Presentation()
+        title_slide_layout = self.presantation.slide_layouts[0]
+        slide = self.presantation.slides.add_slide(title_slide_layout)
+        title = slide.shapes.title
+        subtitle = slide.placeholders[1]
+
+        title.text = "Hello, World!"
+        subtitle.text = "Презентация создана"
+        self.presantation.save('Презентация1.pptx')
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())

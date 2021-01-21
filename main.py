a45a0eed7a8400e42026bf0131730182a414fd39
Roman <rv.reshetko@gmail.com>
2021-01-21 16:53:00 +0300

    Со вторым окном

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from PyQt5 import Qt, QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import QPushButton
 import sys
-from pptx import Presentation
 from  PyQt5.QtGui import QPainter, QColor
 
 
@@ -34,20 +33,30 @@ class GreetingWindow(QtWidgets.QMainWindow):
         self.pushButton = QPushButton(self)
         self.pushButton.setText('Создать презентацию')
         font = QtGui.QFont()
+        font.setFamily('comic sans ms')  # меняет тип шрифта
         font.setPointSize(28)
         self.pushButton.setFont(font)
-        self.pushButton.setGeometry(190, 600, 720, 80)
+        self.pushButton.setGeometry(190, 500, 720, 300)
+        self.pushButton.setStyleSheet(('''QPushButton {            
+                                    background: rgb(184,232,176);
+                                    position: absolute;
+                                    border-radius: 70px;
+                                    padding: 10px;
+                                    color: rgb(255, 255, 255);
+                                }'''))
         self.pushButton.clicked.connect(self.create_presentation)
 
         self.label = QtWidgets.QLabel(self)
         self.label.setGeometry(QtCore.QRect(185, 300, 800, 111))
         self.label.setText('Добро пожаловать!')
         font = QtGui.QFont()
-        font.setPointSize(60)
-        self.label.setFont(font)
+        font.setPointSize(200)
+        self.label.setFont(QtGui.QFont("comic sans ms", 40, QtGui.QFont.Bold))
+        self.label.setStyleSheet("color: rgb(184,232,176);")
+        self.label.adjustSize() #???
 
     def create_presentation(self):
-        self.k = MainScreen([Slide(111, title='Привет')])
+        self.k = ChooseTemplate([Slide(111, title='Привет')])
         self.k.show()
         self.hide()
 
@@ -76,8 +85,17 @@ class MainScreen(QtWidgets.QMainWindow):
 
         self.pushButton = QPushButton('Готово', self)
         self.pushButton.setGeometry(20, 850, 90, 40)
+        self.pushButton.setStyleSheet('''QPushButton {            
+                                    background: rgb(184,232,176);
+                                    border-style: outset;
+                                    border-width: 2px;
+                                    border-radius: 15px;
+                                    border-color: rgb(49,106,60) ;
+                                    padding: 4px;
+                                    color: rgb(255, 255, 255);
+                                }''')   # параметры кнопки
         font = QtGui.QFont()
-        font.setPointSize(18)
+        font.setPointSize(12)
         self.pushButton.setFont(font)
         self.pushButton.clicked.connect(self.create_presentation)
         self.pushButton.show()
@@ -87,6 +105,15 @@ class MainScreen(QtWidgets.QMainWindow):
         for i in range(self.number_of_slides):
             x = QtWidgets.QPushButton(self)
             x.setGeometry(10, 70*i, 160, 100)
+            x.setStyleSheet('''QPushButton {
+                                    background: rgb(95,193,215);
+                                    border-style: outset;
+                                    border-width: 2px;
+                                    border-radius: 25px;
+                                    border-color: white ;
+                                    padding: 4px;
+                                    color: rgb(255, 255, 255);
+                                }''')
             font = QtGui.QFont()
             font.setPointSize(14)
             x.setFont(font)
@@ -97,6 +124,15 @@ class MainScreen(QtWidgets.QMainWindow):
 
         x = QtWidgets.QPushButton(self)
         x.setGeometry(10, 70*self.number_of_slides, 160, 100)
+        x.setStyleSheet('''QPushButton {
+                                    background: rgb(95,193,215);
+                                    border-style: outset;
+                                    border-width: 2px;
+                                    border-radius: 25px;
+                                    border-color: white ;
+                                    padding: 4px;
+                                    color: rgb(255, 255, 255);
+                                }''')
         font = QtGui.QFont()
         font.setPointSize(50)
         x.setFont(font)
@@ -110,6 +146,7 @@ class MainScreen(QtWidgets.QMainWindow):
 
         mw = Qt.QScrollArea(self)
         mw.setWidget(w)
+        mw.setStyleSheet('background: rgb(95,193,215);')
         mw.resize(200, 830)
         mw.show()
 
@@ -125,6 +162,31 @@ class MainScreen(QtWidgets.QMainWindow):
 
     def create_presentation(self):
         print(1)
+
+class ChooseTemplate(MainScreen):
+    def __init__(self, slides):
+        super().__init__(slides)
+        self.tempinit()
+
+    def tempinit(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setGeometry(QtCore.QRect(250, 50, 300, 70))
+        self.label.setText('Выберите макет слайда')
+        font = QtGui.QFont()
+        font.setPointSize(60)
+        self.label.setFont(QtGui.QFont("comic sans ms", 30, QtGui.QFont.Bold))
+        self.label.setStyleSheet("color: rgb(184,232,176);")
+        self.label.adjustSize()
+
+        tmp1 = QPushButton("1 template", self)
+        tmp1.setGeometry(300, 200, 200, 100)
+        tmp2 = QPushButton("2 template", self)
+        tmp2.setGeometry(750, 200, 200, 100)
+        tmp3 = QPushButton("3 template", self)
+        tmp3.setGeometry(300, 650, 200, 100)
+        tmp4 = QPushButton("4 template", self)
+        tmp4.setGeometry(750, 650, 200, 100)
+
 
 
 if __name__ == '__main__':
